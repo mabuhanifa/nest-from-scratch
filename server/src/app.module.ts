cd8aee9f9c52257f8c0d336a6ead33ce1d85603b
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-04 04:35:41 +0600

    database connection working

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.local.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        ssl: configService.get<boolean>('DATABASE_SSL'),
+        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+        logging: configService.get<boolean>('DATABASE_LOGGING'),
+        database: configService.get('DATABASE_NAME'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
