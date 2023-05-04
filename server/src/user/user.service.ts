45434c91f2f9a3eea66839c79be9edfa9ef8252b
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-04 07:05:58 +0600

    Constants

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserRepository } from './repo/user.repository';
 
 @Injectable()
 export class UserService {
+  constructor(private userRepository: UserRepository) {}
+
   create(createUserDto: CreateUserDto) {
+    const user: User = new User();
+    user.email = createUserDto.email;
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+    user.password = createUserDto.password;
     return 'This action adds a new user';
   }
 
