5818e5a2858b8a5bbe4e27baa00b9bdf66a75e18
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-05 14:46:47 +0600

    layout setup working

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,10 +1,14 @@
+import Nav from "./Nav";
 import SideBar from "./SideBar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex ">
-      <SideBar />
-      <main className="">{children}</main>
+    <div className="bg-white text-gray-600 my-10 px-5 xl:mx-20">
+      <Nav />
+      <div className="flex ">
+        <SideBar />
+        <main className="">{children}</main>
+      </div>
     </div>
   );
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,5 @@
 import Layout from "@/components/Layout";
-import Nav from "@/components/Nav";
 
 export default function Home() {
-  return (
-    <div className="bg-white text-gray-600 my-10 px-5 xl:mx-20">
-      <Nav/>
-      <Layout>
-        DashBoard
-      </Layout>
-    </div>
-  );
+  return <Layout>DashBoard</Layout>;
 }
