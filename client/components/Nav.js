3a612662dae5dfc4a4bc758c639318a48edc5693
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-05 13:53:30 +0600

    user icon

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,3 +1,5 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { CiUser } from "react-icons/ci";
 import { RiSearchLine } from "react-icons/ri";
 import { TiTick } from "react-icons/ti";
 
@@ -10,13 +12,24 @@ export default function Nav() {
           Todos
         </span>
       </div>
-      <div>
+      <div className="flex items-center">
         <div className="relative">
-            <input type="text" className="bg-gray-200 rounded-sm py-1 w-96 px-10"/>
-            <RiSearchLine className="text-gray-400 text-2xl absolute left-2 top-1"/>
+          <input
+            type="text"
+            className="bg-gray-200 rounded-sm py-1.5 w-96 px-10"
+            placeholder="Search"
+          />
+          <RiSearchLine className="text-gray-400 text-2xl absolute left-2 top-1.5" />
+        </div>
+        <div>
+          <button className="px-5 mt-1">
+            <AiOutlinePlus className="text-2xl text-gray-600" />
+          </button>
         </div>
       </div>
-      <div></div>
+      <div >
+        <CiUser className="text-2xl text-gray-600"/>
+      </div>
     </nav>
   );
 }
