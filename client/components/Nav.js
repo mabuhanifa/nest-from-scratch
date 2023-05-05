cc09b91ece573b4bd5ae7a8d2f712a0da43dfff9
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-05 13:38:21 +0600

    shadow

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,16 +1,20 @@
-import { TiTick } from 'react-icons/ti'
-
+import { TiTick } from "react-icons/ti";
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="shad rounded-lg py-3 px-5 flex justify-between items-center">
+      <div className="flex items-center justify-between">
+        <span className="font-bold">
+          <TiTick className="text-blue-500 inline text-2xl mb-1" />
+          Todos
+        </span>
+      </div>
+      <div>
         <div>
-            <span>
-                <TiTick className='text-blue-500'/>Todo
-            </span>
+            <input type="text" className="bg-gray-200 rounded-sm py-1 "/>
         </div>
-        <div></div>
-        <div></div>
+      </div>
+      <div></div>
     </nav>
-  )
+  );
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <div className="bg-white text-gray-600">
+    <div className="bg-white text-gray-600 my-10 xl:mx-20">
       <Nav />
     </div>
   );
