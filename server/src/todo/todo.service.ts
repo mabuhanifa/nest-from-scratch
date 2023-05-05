f4931521bcf062944b274e4c178a9747314604a7
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-05 10:45:50 +0600

    TypeOrmModule

--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
+  constructor(
+    @InjectRepository(Todo) private todoRepository: Repository<Todo>,
+  ) {}
   create(createTodoDto: CreateTodoDto) {
     return 'This action adds a new todo';
   }
