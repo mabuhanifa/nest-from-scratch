41078edc333ec84ca6b88f26b1d5524dc213de4a
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-05 10:40:29 +0600

    todo entity

--- server/src/todo/entities/todo.entity.ts
+++ server/src/todo/entities/todo.entity.ts
@@ -1,1 +1,19 @@
-export class Todo {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  completed: boolean;
+
+  @Column()
+  date: string;
+}
