68620030247c348eaa2c7f37eb895d632c416386
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-06 17:11:11 +0600

    modal updated

--- client/components/Modal.js
+++ client/components/Modal.js
@@ -10,17 +10,30 @@ export default function Modal({ view, setModal }) {
   return (
     <div
       id="container"
-      className="mx-5 bg-opacity-30 backdrop-blur-sm fixed inset-0 flex justify-center z-15 py-10"
+      className="mx-5 bg-opacity-30 backdrop-blur fixed inset-0 flex justify-center z-15 py-10"
       onClick={closeModal}
     >
-      <div className="max-w-2xl h-80 rounded-xl p-10 relative shad">
-        <h1 className="text-2xl font-bold mb-10">name</h1>
+      <div className="max-w-2xl rounded-xl p-10 relative shad h-max">
+        <h1 className="text-2xl font-bold mb-10">Create a new Todo</h1>
         <button
           className=" text-red-500 absolute top-2 right-2 bg-red-200 p-2 rounded-full hover:bg-red-300"
           onClick={() => setModal((m) => !m)}
         >
           <VscChromeClose className="text-xl" />
         </button>
+        <div>
+          <input type="text" placeholder="Title" className="p-2 bg-slate-200" />
+          <br />
+          <textarea
+            type="text"
+            placeholder="Description"
+            className="p-2 bg-slate-200 my-5"
+          />
+          <br />
+          <input type="text" placeholder="Data" className="p-2 bg-slate-200" />
+          <br />
+        <button className="bg-blue-500 px-10 text-white p-2 rounded w-full mt-10">Create</button>
+        </div>
       </div>
     </div>
   );
