a1411e8c01e04a009ccb6270085b182ff7134559
mabuhanifa <moh.abuhanifa@gmail.com>
2024-05-23 18:54:09 +0600

    user controllers

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+   */
+
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Get('interns')
+  findAllInterns() {
+    return [];
+  }
+}
